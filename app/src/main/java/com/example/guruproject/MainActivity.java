741faa5d268d50db650708b35201d7c1b44ae38f
seunghyae <dltmdgpgp@naver.com>
2019-07-04 16:22:32 +0900

    0704까지

--- app/src/main/java/com/example/guruproject/MainActivity.java
+++ app/src/main/java/com/example/guruproject/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.guruproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn_reg=findViewById(R.id.Join);
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),activity_Join.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_login=findViewById(R.id.LogIn);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),MemoActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
