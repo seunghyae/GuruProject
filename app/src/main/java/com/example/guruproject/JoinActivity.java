e934045c9305a6757f0f59713dcb9dd902c909c4
seunghyae <dltmdgpgp@naver.com>
2019-07-08 15:55:30 +0900

    filedb 수정

--- app/src/main/java/com/example/guruproject/JoinActivity.java
+++ app/src/main/java/com/example/guruproject/JoinActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -19,88 +18,59 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import com.example.guruproject.database.Database;
-import com.example.guruproject.model.MemberModel;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static androidx.core.content.FileProvider.getUriForFile;
-
 public class JoinActivity extends AppCompatActivity {
-    private final String TAG = this.getClass().getSimpleName(); // 클래스명 획득
-    public static final int VIEW_DETAIL = 100;  // 상세화면 식별자
-    public static final int VIEW_SAVE = 101;  // 저장화면 식별자
 
     private static final int MY_PERMISSION_CAMERA = 1111;
     private static final int REQ_TAKE_PHOTO = 2222;
     private static final int REQ_TAKE_ALBUM = 3333;
     private static final int REQ_TAKE_IMAGE_CROP = 4444;
 
-    EditText regId;
-    EditText regPwd;
-    EditText regName;
-
-    Button btn_save;
-
-    Database db;
-
-    Button btnCamera;
-    ImageView imgView;
-
-
+    //멤버변수
+    private ImageView mImgProfile;
+    private EditText mEdtId, mEdtName, mEdtPw1, mEdtPw2;
 
     private String mCurrentImageFilePath = null;
     private Uri mProviderUri = null;
     private Uri mPhotoUri = null;
     private Uri mAlbumUri = null;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 권한 체크
+
         checkPermission();
+
         setContentView(R.layout.activity_join);
-
-        db= Database.getInstance(getApplicationContext());
-
-        regId = findViewById(R.id.regId);
-        regPwd = findViewById(R.id.regPwd);
-        regName = findViewById(R.id.regName);
-
-        btn_save=findViewById(R.id.btn_save);
-        btn_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MemberModel member = new MemberModel();
-                member.setId(regId.getText().toString());
-                member.setPwd(regPwd.getText().toString());
-                member.setName(regName.getText().toString());
-                db.setMember(member);
-                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // XML 바인딩
-        imgView = findViewById(R.id.imgView);
-        btnCamera = findViewById(R.id.btnCamera);
-
-        btnCamera.setOnClickListener(new View.OnClickListener() {
+        mImgProfile = findViewById(R.id.imgProfile);
+        mEdtId = findViewById(R.id.edtId);
+        mEdtName = findViewById(R.id.edtName);
+        mEdtPw1 = findViewById(R.id.edtPw1);
+        mEdtPw2 = findViewById(R.id.edtPw2);
+        //카메라 버튼
+        findViewById(R.id.btnCamera).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 captureCamera();
             }
         });
+        //회원가입 버튼
+        findViewById(R.id.btnJoin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                joinProcess();
+            }
+        });
+    }//end onCreate()
 
-
-
-
-    }
-    // 카메라 어플리케이션 호출
-    public void captureCamera() {
+    //카메라 작업시작
+    private void captureCamera() {
         String state = Environment.getExternalStorageState();
 
         if (!Environment.MEDIA_MOUNTED.equals(state)) {
@@ -112,7 +82,7 @@ public class JoinActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null) {
             File photoFile = createFileName(); // 저장할 파일
             if (photoFile != null) {
-                Uri providerURI = getUriForFile(this, getPackageName(), photoFile);
+                Uri providerURI = FileProvider.getUriForFile(this, getPackageName(), photoFile);
                 mProviderUri = providerURI;
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, providerURI);
 
@@ -121,45 +91,15 @@ public class JoinActivity extends AppCompatActivity {
         }
     }
 
-    // 앨범 어플리케이션 호출
-    private void getAlbum() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-        startActivityForResult(intent, REQ_TAKE_ALBUM);
+    //회원가입 작업시작
+    private void joinProcess() {
+
     }
 
-    // 카메라, 앨범등의 처리결과
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        Database db = Database.getInstance(this);
 
-        switch (requestCode) {
-
-            case REQ_TAKE_PHOTO:
-                if (resultCode == Activity.RESULT_OK) {
-
-
-                    imgView.setImageURI(mProviderUri); // 사진촬영한 이미지 설정
-
-                } else {
-                    Toast.makeText(this, "사진촬영을 취소하였습니다.", Toast.LENGTH_LONG).show();
-                }
-                break;
-            case REQ_TAKE_ALBUM:
-                if (resultCode == Activity.RESULT_OK) {
-                    File albumFile = createFileName();
-                    mPhotoUri = data.getData();
-                    mAlbumUri = Uri.fromFile(albumFile);
-
-                    imgView.setImageURI(mPhotoUri);   // 앨범에서 선택한 이미지 설정
-                }
-                break;
-        } // End switch
-    }
 
     // 이미지 파일명 생성
-    private File createFileName(){
+    private File createFileName() {
         // 현재 "년월일 시분초"를 기준으로 파일명 생성
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String fileName = timeStamp + ".jpg";
@@ -175,7 +115,7 @@ public class JoinActivity extends AppCompatActivity {
         return imageFile;
     }
 
-    private void checkPermission(){
+    private void checkPermission() {
         // Self 권한 체크
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
@@ -189,6 +129,7 @@ public class JoinActivity extends AppCompatActivity {
                     || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                     || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)
             ) {
+                //다이얼로 출력
 
             } else {
                 // 권한동의 팝업 표시 요청
@@ -215,4 +156,34 @@ public class JoinActivity extends AppCompatActivity {
             default:
         } // End switch
     }
-}
+
+
+    // 카메라, 앨범등의 처리결과
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        switch (requestCode) {
+
+            case REQ_TAKE_PHOTO:
+                if (resultCode == Activity.RESULT_OK) {
+
+                    mImgProfile.setImageURI(mProviderUri); // 사진촬영한 이미지 설정
+
+                } else {
+                    Toast.makeText(this, "사진촬영을 취소하였습니다.", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case REQ_TAKE_ALBUM:
+                if (resultCode == Activity.RESULT_OK) {
+                    File albumFile = createFileName();
+                    mPhotoUri = data.getData();
+                    mAlbumUri = Uri.fromFile(albumFile);
+
+                    mImgProfile.setImageURI(mPhotoUri);   // 앨범에서 선택한 이미지 설정
+                }
+                break;
+        } // End switch
+    }
+
+
+}//end class
--- app/src/main/java/com/example/guruproject/LoginActivity.java
+++ app/src/main/java/com/example/guruproject/LoginActivity.java
@@ -1,61 +1,71 @@
 package com.example.guruproject;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.guruproject.database.Database;
+import com.example.guruproject.R;
+import com.example.guruproject.bean.MemberBean;
+import com.example.guruproject.db.FileDB;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText editId;
-    EditText editPassword;
 
-    Database db;
-
-    TextView txtvResult;
+    //멤버변수 자리
+    private EditText mEdtId, mEdtPw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        db=Database.getInstance(getApplicationContext());
+        mEdtId = findViewById(R.id.edtId);
+        mEdtPw = findViewById(R.id.edtPw);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnJoin = findViewById(R.id.btnJoin);
 
-        editId = findViewById(R.id.editID);
-        editPassword=findViewById(R.id.editPassword);
+        btnLogin.setOnClickListener(mBtnLoginClick);
+        btnJoin.setOnClickListener(mBtnJoinClick);
 
-        txtvResult=findViewById(R.id.txtvResult);
+    }//end onCreate()
 
-        Button btn_reg=findViewById(R.id.Join);
-        btn_reg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), JoinActivity.class);
-                startActivity(intent);
+    //로그인 버튼 클릭 이벤트
+    private View.OnClickListener mBtnLoginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String memId = mEdtId.getText().toString();
+            String memPw = mEdtPw.getText().toString();
+
+            MemberBean memberBean = FileDB.getFindMember(LoginActivity.this, memId);
+            if(memberBean == null) {
+                Toast.makeText(LoginActivity.this, "해당 아이디는 가입이 되어 있지 않습니다.", Toast.LENGTH_LONG).show();
+                return;
             }
-        });
-
-        Button btn_login=findViewById(R.id.LogIn);
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                boolean check = db.checkMember(editId.getText().toString(), editPassword.getText().toString());
-                if(check==true){
-                    txtvResult.setText("체크결과=로그인성공");
-
-                    Intent intent= new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
-                }else{
-                    txtvResult.setText("체크결과=로그인실패");
-                }
+            //패스워드 비교
+            if( TextUtils.equals(memberBean.memPw, memPw) ) {
+                FileDB.setLoginMember(LoginActivity.this, memberBean); //저장
+                //비밀번호 일치
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            } else {
+                Toast.makeText(LoginActivity.this, "패스워드가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
+                return;
             }
-        });
+        }
+    };
 
-    }
-
+    //회원가입 버튼 클릭 이벤트
+    private View.OnClickListener mBtnJoinClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent ii = new Intent(LoginActivity.this, CameraCapture2Activity.class);
+            startActivity(ii);
+        }
+    };
 }
